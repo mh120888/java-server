4a50abdecf17d008b530d5227c207c6985409f36
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-07-11 10:06:18 -0700

    Adds NotFoundEndpoint to implement 404 response

--- CoffeeEndpoint.java
+++ CoffeeEndpoint.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
  */
 public class CoffeeEndpoint {
     public static HashMap<String, String> getResponseData(HashMap<String, String> requestData) {
+        HashMap<String, String> responseData = new HashMap<>();
         String method = requestData.get("method");
         String path = requestData.get("path");
 
-        HashMap<String, String> responseData = new HashMap<>();
         if (path.equals("/coffee")) {
             responseData.put("responseLine", "HTTP/1.1 418");
             responseData.put("body", "I'm a teapot");
