e270edca3f447120c43c02328d981245cacce296
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-22 14:06:54 -0700

    Trying to run program with an odd number of arguments writes usage to stderr and exits.

--- src/main/java/com/github/mh120888/server/CommandLineArgsParser.java
+++ src/main/java/com/github/mh120888/server/CommandLineArgsParser.java
@@ -3,17 +3,37 @@ package com.github.mh120888.server;
 import java.util.HashMap;
 
 public class CommandLineArgsParser {
+    private static String usage = "Usage: java -jar javaserver-1.0-SNAPSHOT.jar [-pd]\n" +
+            "\t-p <port number> {80}\n" +
+            "\t-d <public directory> {.}";
+
     public static HashMap<String, String> groupOptions(String[] args) {
         HashMap<String, String> parsedArgs = new HashMap<>();
-        if ((args.length % 2) == 0) {
-            for (int i = 0; i < args.length - 1; i += 2) {
-                String flag = args[i];
-                if (flag.charAt(0) == '-') {
-                    parsedArgs.put(flag, args[i + 1]);
-                }
-            }
+        if (areArgsInPairs(args)) {
+            addOptionsToParsedArgs(args, parsedArgs);
+        } else {
+            printUsageAndExit();
         }
 
         return parsedArgs;
     }
+
+    static void printUsageAndExit() {
+        System.err.println(usage);
+        System.exit(64);
+    }
+
+    private static void addOptionsToParsedArgs(String[] args, HashMap<String, String> parsedArgs) {
+        for (int i = 0; i < args.length - 1; i += 2) {
+            String flag = args[i];
+            if (flag.charAt(0) == '-') {
+                parsedArgs.put(flag, args[i + 1]);
+            }
+        }
+    }
+
+    private static boolean areArgsInPairs(String[] args) {
+        return (args.length % 2) == 0;
+    }
 }
+
--- src/test/java/com/github/mh120888/server/CommandLineArgsParserTest.java
+++ src/test/java/com/github/mh120888/server/CommandLineArgsParserTest.java
@@ -1,10 +1,50 @@
 package com.github.mh120888.server;
 
+import org.junit.After;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.security.Permission;
+import java.security.Security;
 import java.util.HashMap;
 import static org.junit.Assert.*;
 
 public class CommandLineArgsParserTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private SecurityManager securityManager;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        MyServer.myPort = 3000;
+        MyServer.publicDirectory = "~/";
+
+        securityManager = System.getSecurityManager();
+        System.setSecurityManager(new SecurityManager() {
+            @Override
+            public void checkPermission(Permission perm) {
+            }
+
+            @Override
+            public void checkPermission(Permission perm, Object context) {
+            }
+
+            @Override
+            public void checkExit(int status) {
+                String message = "System exit requested with error " + status;
+                throw new SecurityException(message);
+            }
+        });
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setSecurityManager(securityManager);
+    }
+
     @Test
     public void groupOptionsMatchesSingleFlagWithValue() {
         String[] args = {"-f",  "somethingRandom"};
@@ -41,10 +81,41 @@ public class CommandLineArgsParserTest {
     }
 
     @Test
-    public void groupOptionsReturnsAnEmptyHashmapIfGivenAnArrayOfOddLength() {
-        String[] args = {"-oneflag", "secondValue",  "-flagWithNoValue"};
-        HashMap<String, String> parsedArgs = CommandLineArgsParser.groupOptions(args);
+    public void groupOptionsReturnsAnEmptyHashmapIfArgsAreNotInPairs() {
+        String[] args = {"-oneflag", "secondValue", "-flagWithNoValue"};
+        HashMap<String, String> parsedArgs = new HashMap<>();
+        try {
+            parsedArgs = CommandLineArgsParser.groupOptions(args);
+        } catch (SecurityException e) {
 
-        assertEquals(0, parsedArgs.size());
+        } finally {
+            assertEquals(0, parsedArgs.size());
+        }
     }
+
+    @Test
+    public void groupOptionsWillExitWithStatusCode64IfArgsAreNotInPairs() {
+        SecurityException exception = null;
+        try {
+            CommandLineArgsParser.printUsageAndExit();
+        } catch (SecurityException e) {
+            exception = e;
+        } finally {
+            assertEquals("System exit requested with error 64", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void groupOptionsDisplaysErrorMessageIfArgsAreNotInPairs() {
+        String[] args = {"-oneflag", "secondValue",  "-flagWithNoValue"};
+
+        try {
+            CommandLineArgsParser.groupOptions(args);
+        } catch (SecurityException e) {
+
+        } finally {
+            assertFalse(errContent.toString().isEmpty());
+        }
+    }
+
 }
