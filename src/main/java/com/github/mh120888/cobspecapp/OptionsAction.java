77d49616b85fa7f71f08faa6d50e4ef0e9c9b4c4
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-08-31 13:34:09 -0700

    Replaces switch statement with HashMap for determining permitted methods in OptionsAction.

--- src/main/java/com/github/mh120888/cobspecapp/OptionsAction.java
+++ src/main/java/com/github/mh120888/cobspecapp/OptionsAction.java
@@ -1,14 +1,24 @@
 package com.github.mh120888.cobspecapp;
 
+import com.github.mh120888.httpmessage.HTTPHeaders;
 import com.github.mh120888.httpmessage.HTTPRequest;
 import com.github.mh120888.httpmessage.HTTPResponse;
 import com.github.mh120888.httpmessage.HTTPStatus;
 
+import java.util.HashMap;
+
 public class OptionsAction implements Action {
+
+    private final static String[] defaultMethods = new String[0];
+    private final static HashMap<String, String[]> permittedMethods = new HashMap<>();
+    static {
+        permittedMethods.put("/method_options", new String[] {"GET","HEAD","POST","OPTIONS","PUT"});
+        permittedMethods.put("/method_options2", new String[] {"GET","OPTIONS"});
+    }
 
     public HTTPResponse getResponse(HTTPRequest request, HTTPResponse response) {
         response.setStatus(HTTPStatus.OK);
-        response.addHeader("Allow", buildHeaders(request.getPath()));
+        response.addHeader(HTTPHeaders.ALLOW, buildHeaders(request.getPath()));
 
         return response;
     }
@@ -20,9 +30,6 @@ public class OptionsAction implements Action {
     }
 
     private String[] acceptedMethods(String path) {
-        switch (path) {
-            case "/method_options": return new String[] {"GET","HEAD","POST","OPTIONS","PUT"};
-            default:                return new String[] {"GET","OPTIONS"};
-        }
+        return permittedMethods.getOrDefault(path, defaultMethods);
     }
 }
